8a0b2de8975b89d262340c4b4a8c81562ba9fa80
netcharger <suresh.bhavani123@gmail.com>
2025-12-22 23:26:22 +0530

    added packages to req text

--- panchang_api/settings.py
+++ panchang_api/settings.py
@@ -8,12 +8,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-# Use PyMySQL instead of mysqlclient (easier on Windows)
-try:
-    import pymysql
-    pymysql.install_as_MySQLdb()
-except ImportError:
-    pass
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
