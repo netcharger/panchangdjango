d6012350ce84942fe812d8bf4ae7c636b8152375
netcharger <suresh.bhavani123@gmail.com>
2025-12-23 08:56:53 +0530

    start sh

--- start.sh
+++ start.sh
@@ -1,27 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Waiting for MySQL..."
+echo "Running migrations..."
+python manage.py migrate --noinput
 
-python - <<'EOF'
-import os, time, socket
-from urllib.parse import urlparse
-
-u = urlparse(os.environ["DATABASE_URL"])
-host = u.hostname
-port = u.port or 3306
-
-print(f"Using MySQL at {host}:{port}")
-
-while True:
-    try:
-        s = socket.create_connection((host, port), 2)
-        s.close()
-        break
-    except Exception as e:
-        print("Waiting for MySQL...", e)
-        time.sleep(2)
-EOF
-
-python manage.py migrate
+echo "Starting Gunicorn..."
 exec gunicorn panchang_api.wsgi:application --bind 0.0.0.0:8000
