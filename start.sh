a69935f0a89a2035bba560867fff1515762bf7af
netcharger <suresh.bhavani123@gmail.com>
2026-03-07 20:10:27 +0530

    fix: use --fake-initial to fix festivals table already exists on coolify

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running migrations..."
-python manage.py migrate --noinput
+python manage.py migrate --fake-initial --noinput
 
 echo "Starting Gunicorn..."
 exec gunicorn panchang_api.wsgi:application --bind 0.0.0.0:8000
